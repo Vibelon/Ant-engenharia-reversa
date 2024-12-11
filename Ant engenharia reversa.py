089581e8eed543a8ef630b5156ec1e18ce575684
Vibelo <vibelo_git@outlook.com>
2024-12-11 19:09:55 -0300

    Update Ant engenharia reversa.py

--- Ant engenharia reversa.py
+++ Ant engenharia reversa.py
@@ -2,13 +2,13 @@ import random
 
 with open('código.txt', 'r') as arquivo:
     código = arquivo.read()
-
+intensidade = int(input("digite o quão poderosa deseja que seja a obfuscação: "))
 nomes = input("Digite o nome de todas as variáveis, instâncias de objeto etc. de seu código separadas por espaço: ")
 nomes = nomes.split()
 z = ""
 nomes_alterados = []
 for x in range(len(nomes)):
-    for y in range(0,1000):
+    for y in range(0,intensidade):
         z = z + str(random.randint(1000,2000))
     z = "a" + z
     nomes_alterados.append(z)
